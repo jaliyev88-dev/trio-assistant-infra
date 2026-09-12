462ef4febd8444e9144399aa9e7db9f570a6fa81
jaliyev88-dev <jaliyev88@gmail.com>
2026-09-13 00:11:33 +0400

    Update setup_caddy.sh

--- setup_caddy.sh
+++ setup_caddy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
-/snap/bin/caddy adapt --config /root/trio-assistant-infra/Caddyfile --adapter caddyfile > /var/snap/caddy/common/caddy.json
+cp /root/trio-assistant-infra/Caddyfile /var/snap/caddy/common/Caddyfile
+/snap/bin/caddy adapt --config /var/snap/caddy/common/Caddyfile --adapter caddyfile > /var/snap/caddy/common/caddy.json
 snap restart caddy
 echo "Caddy JSON regenerated and service restarted."
